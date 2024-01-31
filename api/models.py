9722df8f96640f199b747f03eae13cf2c173e794
Boyko Alex <elorian@yandex.ru>
2024-01-31 21:35:14 +0300

    Add DRF and api func

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField('Тело файла')
+    uploaded_at = models.DateTimeField('когда скачено', auto_now_add=True)
+    processed = models.BooleanField('Обработан', default=False)
+
